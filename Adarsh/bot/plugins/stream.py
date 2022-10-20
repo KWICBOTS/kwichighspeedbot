eb2542ae76352da345ff66fa86dfbd33d004bde6
kwichighspeedbot <116018629+kwichighspeedbot@users.noreply.github.com>
2022-10-20 20:00:48 +0530

    Update stream.py

--- Adarsh/bot/plugins/stream.py
+++ Adarsh/bot/plugins/stream.py
@@ -64,7 +64,7 @@ async def private_receive_handler(c: Client, m: Message):
     if Var.UPDATES_CHANNEL != "None":
         try:
             user = await c.get_chat_member(Var.UPDATES_CHANNEL, m.chat.id)
-            if user.status == "banned":
+            if user.status == "kicked":
                 await c.send_message(
                     chat_id=m.chat.id,
                     text="𝚈𝙾𝚄 𝙰𝚁𝙴 𝙱𝙰𝙽𝙽𝙴𝙳../**",
